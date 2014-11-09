a290415e8cbe09220b2f10254c819f56414828f0
ZetaChow <banmuhuangci@gmail.com>
2014-11-09 13:34:56 +0800

    no message

--- runtext.go
+++ runtext.go
@@ -20,23 +20,23 @@ func main() {
 	fromEncoding := flag.String("ef", "utf-8", "文档的编码，默认'utf-8'")
 	toEncoding := flag.String("et", "utf-8", "欲转换为的编码，默认'utf-8'")
 	cmd := flag.String("cmd", "", "外部命令，每一行读取完毕后执行的命令，会将行内容传递给命令作为参数，默认无命令")
+	isAll := false
+	/*扩展*/
 
 	flag.Parse()
 
-	//path := "/Users/banmuhuangci/Downloads/logfile.bak"
-	//path := "/Users/banmuhuangci/Desktop/非法营运.txt"
-	//isAll, count, substr, linenum := true, 1, "", true
-
-	isAll := false
 	if *count == 0 {
 		isAll = true
 	}
 
 	selectTxt(*path, isAll, *ignore, *count, *substr, *linenum, *fromEncoding, *toEncoding, *cmd)
+	fmt.Println("Read Over!!")
 
 }
 
 func selectTxt(path string, isAll bool, ignore int, count int, substr string, linenum bool, fromEncoding string, toEncoding string, cmd string) {
+
+	// 打开文件流
 	fp, err := os.Open(path)
 	if err != nil {
 		fmt.Println("[open]:", err.Error())
@@ -45,16 +45,16 @@ func selectTxt(path string, isAll bool, ignore int, count int, substr string, li
 	}
 	defer fp.Close()
 
+	// 创建文件阅读器
 	r := bufio.NewReader(fp)
 
-	//忽略掉指定行数
+	// 忽略掉指定行数
 	for i := 0; i < ignore; i++ {
-		_, _, err := r.ReadLine()
-		if err != nil {
+		if _, _, err := r.ReadLine(); err != nil {
 			if err.Error() != "EOF" {
 				fmt.Println("[ignore line]:", err.Error())
-				break
 			}
+			break
 		}
 	}
 
@@ -72,9 +72,9 @@ func selectTxt(path string, isAll bool, ignore int, count int, substr string, li
 
 		var linestring string
 		if fromEncoding != toEncoding {
-			linestring, _ = iconv.ConvertString(string(line), fromEncoding, toEncoding) // 将byte数组转换为字符串
+			linestring, _ = iconv.ConvertString(string(line), fromEncoding, toEncoding) // 转换编码并转换程字符串
 		} else {
-			linestring = string(line) // 将byte数组转换为字符串
+			linestring = string(line)
 		}
 
 		//判断行中是否有指定包含的内容
@@ -90,7 +90,7 @@ func selectTxt(path string, isAll bool, ignore int, count int, substr string, li
 				command := exec.Command(cmd, linestring)
 				buf, err := command.Output()
 				if err != nil {
-					fmt.Println(err.Error())
+					fmt.Println("[command err]:", err.Error())
 				} else {
 					fmt.Println(cmd, ":", string(buf))
 				}
@@ -99,5 +99,5 @@ func selectTxt(path string, isAll bool, ignore int, count int, substr string, li
 
 		}
 	}
-	fmt.Println("Read Over!!")
+
 }
